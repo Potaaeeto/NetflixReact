18f668c837e783ebad0e72a7085f21edb99e6e8f
Valerie <valerie.ssintasath@gmail.com>
2024-03-19 15:30:27 +0100

    affichage tableau delements

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 // Import d'une image présente quelque part dans le dossier src
 import netflixLogo from "./assets/imgs/netflix-logo.png";
 
-import Movies from "./assets/movies_rnexgr.json";
+import moviesJson from "./assets/movies_rnexgr.json";
 
 function App() {
   // console.log(Movies);
@@ -11,6 +11,17 @@ function App() {
     <>
       <div className="netflix">
         <img className="netflixLogo" src={netflixLogo} alt="Netflix logo" />
+        <div className="scrollbar">
+          {moviesJson.map((elem) => {
+            return <p className="title"> Je suis {elem.category}</p>;
+          })}
+          <div className="movies">
+            {moviesJson.map((elem) => {
+              return <img src={elem.images} alt="moviesPoster" />;
+            })}
+          </div>
+          ;
+        </div>
       </div>
     </>
   );
